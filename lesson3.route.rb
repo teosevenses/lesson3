00c6af6103c8989ee9cb81031f4fd2622d81788e
teosevenses <141769461+teosevenses@users.noreply.github.com>
2023-09-02 23:40:14 +0500

    Add files via upload

---
+++ lesson3.route.rb
@@ -0,0 +1,25 @@
+
+class Route
+attr_reader :stations
+
+#создаем маршрут
+
+def initialize(start_station, last_station)
+@stations = [start_station, last_station]
+end
+
+#добавляем станцию
+def between_station(station)
+  @stations.insert(-1, between_station)
+end
+
+#удаляем станцию
+def between_station(station)
+  @stations.delete(station)
+end
+
+#выводим список всех станций
+def all_stations
+  @stations.each { |station| puts station}
+  end
+end
