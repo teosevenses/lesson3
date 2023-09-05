887a2db40e61dcfec0933054e15d8ab1555f33f1
teosevenses <141769461+teosevenses@users.noreply.github.com>
2023-09-05 19:59:06 +0300

    Update lesson3.station.rb

--- lesson3.station.rb
+++ lesson3.station.rb
@@ -4,29 +4,29 @@ class Station
 attr_accessor :name, :trains
 
   def initialize (name, trains)
-   @name = name
-   @trains = []
-end
+    @name = name
+    @trains = []
+  end
 
 #вводим в пустой массив приезжающий поезд
-def arrive(train)
-@trains << train
-end
+  def arrive(train)
+   @trains << train
+  end
 
 #удаляем из массива поезд который уехал
-def send(train)
-trains.delete(train)
-end
+  def send(train)
+   trains.delete(train)
+  end
 
 #показывает какие поезда на станции
-def show_trains
-trains.each {|train|} train}
-end
+  def show_trains
+   trains.each {|train|} train}
+  end
 
 #делим на грузовые и пассажирские
 
-def trains_types(type) 
-  @trains.select { | train | train.type == type}
+  def trains_types(type) 
+   @trains.select { | train | train.type == type}
   end
 end
 
