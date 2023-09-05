297c7a21a4ee42d090c706d389e8306061be01d1
teosevenses <141769461+teosevenses@users.noreply.github.com>
2023-09-05 19:55:48 +0300

    Update lesson3.route.rb

--- lesson3.route.rb
+++ lesson3.route.rb
@@ -4,22 +4,22 @@ attr_reader :stations
 
 #создаем маршрут
 
-def initialize(start_station, last_station)
+  def initialize(start_station, last_station)
 @stations = [start_station, last_station]
-end
+  end
 
 #добавляем станцию
-def between_station(station)
+  def between_station(station)
   @stations.insert(-1, between_station)
-end
+  end
 
 #удаляем станцию
-def between_station(station)
+  def between_station(station)
   @stations.delete(station)
-end
+  end
 
 #выводим список всех станций
-def all_stations
+  def all_stations
   @stations.each { |station| puts station}
   end
 end
